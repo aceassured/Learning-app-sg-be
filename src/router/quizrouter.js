e240b713a2534af30a072c1a1a48ca794da2d7da
bharath <bharath@aceassured.com>
2025-09-24 10:27:37 +0530

    sendnotification updated

--- src/router/quizrouter.js
+++ src/router/quizrouter.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
 import { getHomeData, reviewSession, startQuiz, submitAnswers, getTopics, startMiniQuiz, startRevisionQuiz } from '../controller/quizcontroller.js';
+import { sendNotificationToUser } from '../services/onesignal.js';
 
 const router = express.Router();
 
@@ -11,6 +12,11 @@ router.post('/start', startQuiz);
 router.post('/startminiquiz', auth, startMiniQuiz);
 router.post('/startrevision', auth, startRevisionQuiz);
 router.post('/submit', submitAnswers);
+router.post('/test-notification/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const result = await sendNotificationToUser(userId, "Test notification");
+  res.json(result)})
+
 router.get('/review/:sessionId', reviewSession);
 
 export default router;
