7989d8bb0a86417c61c5b7f58c42538cf1651ea1
bharath <bharath@aceassured.com>
2025-09-29 18:12:51 +0530

    question updated api

--- src/router/adminrouter.js
+++ src/router/adminrouter.js
@@ -14,7 +14,10 @@ router.post( "/admincreatepoll", auth ,admincreatePoll )
 router.get( "/admingetallpoll", getAllpoll )
 router.post( "/adminannouncementpoll", adminAnnouncementpoll )
 router.post("/particularquestions", getParticularquestions)
-router.put("/editquestion", updatequestion)
+router.put("/editquestion",   upload.fields([
+    { name: "question_url", maxCount: 1 },
+    { name: "answer_file_url", maxCount: 1 }
+  ]),updatequestion)
 router.post("/createsubject", upload.fields([{ name: "icon", maxCount: 1 }]), admincreateSubject)
 router.post("/createtopic", admincreateTopic)
 
